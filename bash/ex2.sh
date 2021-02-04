75ce04dcf0dbfdcfc004e9e7db642d71837a5d0f
Benjamin Verjus <benjamin.verjus@protonmail.com>
2021-02-04 14:00:37 +0100

    Update ex2.sh

--- bash/ex2.sh
+++ bash/ex2.sh
@@ -6,11 +6,11 @@ fi
 
 while [ -n "$1" ]
 do
-    if [ $1 = "robert" ]; then
+    if [ $1 == "robert" ]; then
         echo "Bonjour Robert."
-    elif [ $1 = "test" ]; then
+    elif [ $1 == "test" ]; then
         echo "Attention, ceci est un compte de test."
-    elif [ $1 = "root" ]; then
+    elif [ $1 == "root" ]; then
         echo "Bienvenue administrateur."
     else
         echo "Erreur."
