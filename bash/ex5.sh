00f0cc5566260110055de2122ceb5cab18fbb916
Benjamin Verjus <benjamin.verjus@protonmail.com>
2021-02-24 12:14:45 +0100

    Update

--- bash/ex5.sh
+++ bash/ex5.sh
@@ -2,10 +2,11 @@
 
 if [ $# -ne 1 ]; then
     echo "Ce script doit prendre un argument"
+    exit 1
 fi
 
 file=$1
 
-if [ -e "$file" ]; then
+if [ -e $file ]; then
     echo "Le fichier $file existe"
 fi
