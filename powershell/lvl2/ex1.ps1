fbe0cd790fa1d158e93e57c2f74c250238e3ea19
Benjamin Verjus <benjamin.verjus@protonmail.com>
2021-03-04 17:59:48 +0100

    Update

--- powershell/lvl2/ex1.ps1
+++ powershell/lvl2/ex1.ps1
@@ -1,18 +1,16 @@
 $custom_sentence = ""
 $sentence = Read-Host -Prompt "Entrez une phrase"
- 
-$words_arr = $sentence -split " "
+$words_arr = $sentence -Split " "
  
 foreach($word in $words_arr){
- 
    $reversed_word = "" 
- 
+   
    for ($i=$word.length-1; $i -ge 0; $i--) {
        $reversed_word += $word[$i]  
    } 
- 
+   
    $reversed_word += " "
    $custom_sentence += $reversed_word
 }
- 
+
 Write-Host $custom_sentence -ForegroundColor Green
