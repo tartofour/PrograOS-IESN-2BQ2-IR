9cd0facb87247666483892bbafc89e8b9cde041a
Benjamin Verjus <benjamin.verjus@protonmail.com>
2021-03-18 15:23:29 +0100

    Update

---
+++ powershell/lvl2/ex2.ps1
@@ -0,0 +1,15 @@
+$register_keys = @{"Test-Version"="2.0"; "Test-Region"="Belgium"}
+$log_path = "C:\Users\tartof\Desktop\PowerShell\powershell_lvl2\logfile.log"
+ 
+function Set-RegKey($name, $value){
+    New-ItemProperty -Path HKLM:\SOFTWARE\ -Name $name -Value $value -PropertyType "string" 
+}
+ 
+function Write-Log($message, $path){
+    $message | Out-File -FilePath $path
+}
+ 
+forEach ($key in $register_keys.GetEnumerator()){
+    Set-RegKey -name $key.Name -value $key.Value
+    Write-Log -message "Key added to register = $key.Name : $key.Value" -path $log_path
+}
